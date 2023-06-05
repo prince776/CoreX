94fa7fdbe92d5af818782a4bdd4dbde3ec5d2d2c
Prince Gupta <21282111+prince776@users.noreply.github.com>
2023-06-05 20:03:36 +0530

    Add unique ptr for Arrays

--- tests/unique_ptr_test.cpp
+++ tests/unique_ptr_test.cpp
@@ -7,8 +7,6 @@
 #include <map>
 #include <memory>
 
-std::map<uint64_t, uint64_t> memTracker;
-
 class TrackedMallocator {
   public:
     TrackedMallocator() = default;
@@ -29,12 +27,13 @@ class TrackedMallocator {
         return blk.ptr != nullptr;
     }
 
+    std::map<uint64_t, uint64_t> memTracker{};
+
   private:
     Mallocator mallocator{};
 };
 
-TEST(TestUniquePtr, BasicTests) {
-
+TEST(TestUniquePtr, NonArray) {
     uint64_t allocatedAddr{};
     {
         int val  = 6;
@@ -42,16 +41,17 @@ TEST(TestUniquePtr, BasicTests) {
 
         // Check for allocation.
         allocatedAddr = (uint64_t)ptr.get();
-        EXPECT_EQ(memTracker[allocatedAddr], sizeof(int));
+        EXPECT_EQ(ptr.allocator.memTracker[allocatedAddr], sizeof(int));
 
         EXPECT_EQ(val, *ptr);
         val  = 10;
         *ptr = val;
         EXPECT_EQ(val, *ptr);
-    }
 
-    // Check for free.
-    EXPECT_EQ(false, memTracker.contains(allocatedAddr));
+        // Check for free.
+        ptr.~UniquePtr();
+        EXPECT_EQ(false, ptr.allocator.memTracker.contains(allocatedAddr));
+    }
 
     {
         struct Temp {
@@ -63,7 +63,7 @@ TEST(TestUniquePtr, BasicTests) {
 
         // Check for allocation.
         allocatedAddr = (uint64_t)ptr.get();
-        EXPECT_EQ(memTracker[allocatedAddr], sizeof(Temp));
+        EXPECT_EQ(ptr.allocator.memTracker[allocatedAddr], sizeof(Temp));
 
         EXPECT_EQ(4, ptr->x);
         EXPECT_EQ(5, ptr->y);
@@ -73,8 +73,62 @@ TEST(TestUniquePtr, BasicTests) {
 
         EXPECT_EQ(10, ptr->x);
         EXPECT_EQ(11, ptr->y);
+
+        // Check for free.
+        ptr.~UniquePtr();
+        EXPECT_EQ(false, ptr.allocator.memTracker.contains(allocatedAddr));
+    }
+}
+
+TEST(TestUniquePtr, Array) {
+    uint64_t allocatedAddr{};
+    {
+        int num  = 6;
+        auto ptr = makeUnique<int[]>(TrackedMallocator{}, num);
+
+        // Check for allocation.
+        allocatedAddr = (uint64_t)ptr.get();
+        EXPECT_EQ(ptr.allocator.memTracker[allocatedAddr], sizeof(int) * num);
+
+        EXPECT_EQ(0, *ptr);
+        *ptr   = 1;
+        ptr[1] = 2;
+
+        EXPECT_EQ(1, ptr[0]);
+        EXPECT_EQ(2, ptr[1]);
+
+        // Check for free.
+        ptr.~UniquePtr();
+        EXPECT_EQ(false, ptr.allocator.memTracker.contains(allocatedAddr));
     }
 
-    // Check for free.
-    EXPECT_EQ(false, memTracker.contains(allocatedAddr));
+    {
+        struct Temp {
+            int x, y;
+            Temp() : x(4), y(5) {
+            }
+        };
+
+        int num  = 6;
+        auto ptr = makeUnique<Temp[]>(TrackedMallocator{}, num);
+
+        // Check for allocation.
+        allocatedAddr = (uint64_t)ptr.get();
+        EXPECT_EQ(ptr.allocator.memTracker[allocatedAddr], sizeof(Temp) * num);
+
+        for (int i = 0; i < 5; i++) {
+            EXPECT_EQ(4, ptr[i].x);
+            EXPECT_EQ(5, ptr[i].y);
+        }
+
+        ptr[1].x = 10;
+        ptr[2].y = 12;
+
+        EXPECT_EQ(10, ptr[1].x);
+        EXPECT_EQ(12, ptr[2].y);
+
+        // Check for free.
+        ptr.~UniquePtr();
+        EXPECT_EQ(false, ptr.allocator.memTracker.contains(allocatedAddr));
+    }
 }
