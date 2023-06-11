555dd2e4d9cbefb29fbed3230a3d0cc60e2d57a1
Prince Gupta <21282111+prince776@users.noreply.github.com>
2023-06-11 18:02:50 +0530

    Update stubs

--- include/CoreX/stub.hpp
+++ include/CoreX/stub.hpp
@@ -2,11 +2,15 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
 extern "C" {
-void* malloc(size_t);
-void free(void*) noexcept;
-}
+#endif
 
-extern "C" {
+void* malloc(size_t);
+void free(void*);
+
 void Assert(bool);
+
+#ifdef __cplusplus
 }
+#endif
